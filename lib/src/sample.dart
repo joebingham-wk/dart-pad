b9f8137cfc8b9d6596a06625a7724e1866cbdd26
Joseph Bingham <joe.bingham@workiva.com>
2019-08-09 15:38:53 -0700

    Remove unnecessary import

--- lib/src/sample.dart
+++ lib/src/sample.dart
@@ -10,8 +10,6 @@ import 'dart:html';
 import 'package:react/react_dom.dart' as react_dom;
 import 'package:web_skin_dart/ui_components.dart';
 import 'package:web_skin_dart/ui_core.dart';
-import 'package:web_skin/web_skin.dart';
-
 
 void main() {
   setClientConfiguration();
