4f70ed671fc8774f68b2b5319d04365e17012b54
Keal Jones <keal.jones@workiva.com>
2019-11-15 15:01:13 -0700

    add static asset server

---
+++ bin/serve_prod.dart
@@ -0,0 +1,9 @@
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
+
+void main() {
+  var handler = createStaticHandler('build',
+      defaultDocument: 'index.html');
+
+  io.serve(handler, 'localhost', 8080);
+}
